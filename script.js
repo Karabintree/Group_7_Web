03502717e8d7593a3ccfb668cd25c6dac1a06b8f
Karabintree <rebecabadie@gmail.com>
2024-08-09 18:33:38 -0600

    constchnges in script for images

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
-const BREEDS_URL = 'https://dog.ceo/api/breeds/list/all';
+const BREEDS_URL = "https://dog.ceo/api/breeds/list/all";
 
 // Fetch breeds and populate the dropdown
 async function fetchBreeds() {
-    try {
-        const response = await fetch(BREEDS_URL);
-        const data = await response.json();
-        const breeds = Object.keys(data.message);
+  try {
+    const response = await fetch(BREEDS_URL);
+    const data = await response.json();
+    const breeds = Object.keys(data.message);
 
-        const select = document.querySelector('.breeds');
-        breeds.forEach(breed => {
-            const option = document.createElement('option');
-            option.value = breed;
-            option.textContent = breed;
-            select.appendChild(option);
-        });
-    } catch (error) {
-        console.error('Error fetching breeds:', error);
-    }
+    const select = document.querySelector(".breeds");
+    breeds.forEach((breed) => {
+      const option = document.createElement("option");
+      option.value = breed;
+      option.textContent = breed;
+      select.appendChild(option);
+    });
+  } catch (error) {
+    console.error("Error fetching breeds:", error);
+  }
 }
 
 //  populate the dropdown
 fetchBreeds();
 
-const select = document.querySelector('.breeds');
-const img = document.querySelector('.dog-img');
+const select = document.querySelector(".breeds");
+const imgcontiner = document.querySelector(".img-container");
+const img = document.createElement("img");
 
-select.addEventListener('change', async () => {
-    const breed = select.value;
-    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`);
-    const data = await response.json();
-    img.src = data.message;
+select.addEventListener("change", async () => {
+  const breed = select.value;
+  const response = await fetch(
+    `https://dog.ceo/api/breed/${breed}/images/random`
+  );
+  const data = await response.json();
+  console.log(img);
+  console.log(data.message);
+  img.src = data.message;
+  // const imgcontiner = document.querySelector(".img-container");
+  imgcontiner.appendChild(img);
+  console.log(img);
 });
